f9126db5da7ce90e74ee6f51840ef29dcf273c6d
Andrew Clunis <andrew@orospakr.ca>
2014-11-01 22:58:40 -0400

    Initial Commit

---
+++ suppressor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  suppressor
+//
+//  Created by Andrew Clunis on 2014-11-01.
+//  Copyright (c) 2014 Andrew Clunis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ suppressor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  suppressor
+//
+//  Created by Andrew Clunis on 2014-11-01.
+//  Copyright (c) 2014 Andrew Clunis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
